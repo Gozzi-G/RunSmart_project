69016be3474d60b6cae258b61ebd924556a48af5
Max <max.ivanov6699gmail.com>
2020-05-07 00:45:08 +0300

    add some 'meadia'

--- src/js/script.js
+++ src/js/script.js
@@ -2,15 +2,18 @@ $(document).ready(function () {
    $('.carousel__inner').slick(
       {
          speed: 1500,
-         // adaptiveHeight: true,
+         adaptiveHeight: true,
          prevArrow: '<button type = "button" class = "slick-prev" ><img src="icons/left.svg"></button>',
          nextArrow: '<button type = "button" class = "slick-next" ><img src="icons/right.svg"></button>',
          responsive:[
             {
                breakpoint: 992,
                settings: {
-                  dots: true,
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
                   arrows: false,
+                  dots: true,
+                  dotsClass: 'slick-dots',
                }
             }
          ]
